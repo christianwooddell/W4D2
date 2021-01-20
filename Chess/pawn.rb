844d9173269a3ac24638aecd0f4a79023aa517bf
Christian <cwooddel@u.rochester.edu>
2021-01-20 17:28:03 -0500

    Built on Pawn methods

--- Chess/pawn.rb
+++ Chess/pawn.rb
@@ -2,8 +2,9 @@ require_relative "piece.rb"
 class Pawn < Piece
 
     def initialize(color, board, pos)
-        @steps_forward = 0
+        color == :white ? @steps_forward = pos[0]-1 : @steps_forward = 6 - pos[0] 
         super(color, board, pos)
+
     end 
     
     def symbol 
@@ -14,32 +15,32 @@ class Pawn < Piece
         moves = []
         current_row, current_col = self.pos
 
-        # new_poss = [[current_row + self.forward_dir, current_col], 
-        #  [current_row + self.forward_dir, current_col + self.forward_dir]
-        #  [current_row + self.forward_dir, current_col - self.forward_dir]
-        #  ]
+        new_poss = [[current_row + self.forward_dir, current_col], 
+         [current_row + self.forward_dir, current_col + self.forward_dir],
+         [current_row + self.forward_dir, current_col - self.forward_dir]
+         ]
 
-        # forward = new_poss[0]
-        # left = new_poss[1]
-        # right = new_poss[2]
+        forward = new_poss[0]
+        left = new_poss[1]
+        right = new_poss[2]
 
         
 
-        new_poss.each_with_index
+        new_poss.each_with_index do |new_pos, i|
             if (new_pos[0] >= 0 && new_pos[0] <= 7) && (new_pos[1] >= 0 && new_pos[1] <= 7)
 
-                if self.board[current_row + self.forward_dir, current_col].empty?
-                    moves << [current_row + self.forward_dir, current_col]
-                end 
-                left_piece = self.board[current_row + self.forward_dir, current_col + self.forward_dir]
-                right_piece = self.board[current_row + self.forward_dir, current_col - self.forward_dir]
-                if !left_piece.empty? && left_piece.color != self.color
-                    moves << left_piece.pos
-                end 
-                if !right_piece.empty? && right_piece.color != self.color
-                    moves << right_piece.pos
+                if self.board[new_pos].empty? && i == 0
+                    moves << new_pos
+                
+                # left_piece = self.board[current_row + self.forward_dir, current_col + self.forward_dir]
+                # right_piece = self.board[current_row + self.forward_dir, current_col - self.forward_dir]
+                elsif i == 1 && (!self.board[new_pos].empty? && self.board[new_pos].color != self.color)
+                    moves << new_pos
+                elsif i == 2 && (!self.board[new_pos].empty? && self.board[new_pos].color != self.color)
+                    moves << new_pos
                 end 
             end 
+        end 
         moves
     end 
 
@@ -54,11 +55,12 @@ class Pawn < Piece
     end 
 
     def forward_steps
-        @steps_forward += 1
+        @steps_forward 
     end 
 
     def side_attacks
 
+        self.board[new_pos].color != self.color
     end 
 
 
