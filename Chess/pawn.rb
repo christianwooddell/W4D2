a1c847193cb78277f0b483ff4f5b2b1f2d73df0a
Brekke Green <brekke.andrew.green@gmail.com>
2021-01-20 16:37:39 -0500

    Still working on Pawn

--- Chess/pawn.rb
+++ Chess/pawn.rb
@@ -1,8 +1,6 @@
 require_relative "piece.rb"
 class Pawn < Piece
 
-    WHITE_PAWN_DIRS = [[1,0], [1,1], [1,-1]]
-    BLACK_PAWN_DIRS = [[-1,0], [-1,1], [-1,-1]]
     def initialize(color, board, pos)
         @steps_forward = 0
         super(color, board, pos)
@@ -14,42 +12,37 @@ class Pawn < Piece
 
     def moves 
         moves = []
-        
-        moves = []
         current_row, current_col = self.pos
 
+        # new_poss = [[current_row + self.forward_dir, current_col], 
+        #  [current_row + self.forward_dir, current_col + self.forward_dir]
+        #  [current_row + self.forward_dir, current_col - self.forward_dir]
+        #  ]
 
-        if self.board[current_row + self.forward_dir, current_col].empty? 
-            moves << [current_row + self.forward_dir, current_col]
-        end 
-        left_piece = self.board[current_row + self.forward_dir, current_col + self.forward_dir]
-        right_piece = self.board[current_row + self.forward_dir, current_col - self.forward_dir]
-        if !left_piece.empty? && left_piece.color != self.color
-            moves << left_piece.pos
-        end 
-        if !right_piece.empty? && right_piece.color != self.color
-            moves << right_piece.pos
-        end 
+        # forward = new_poss[0]
+        # left = new_poss[1]
+        # right = new_poss[2]
 
+        
 
-
-            dx, dy = diff
-            new_row = current_row + dx 
-            new_col = current_col + dy
-            new_pos = [new_row, new_col]
+        new_poss.each_with_index
             if (new_pos[0] >= 0 && new_pos[0] <= 7) && (new_pos[1] >= 0 && new_pos[1] <= 7)
-                if self.board[new_pos].empty? || self.board[new_pos].color != self.color
-                    moves << new_pos
+
+                if self.board[current_row + self.forward_dir, current_col].empty?
+                    moves << [current_row + self.forward_dir, current_col]
+                end 
+                left_piece = self.board[current_row + self.forward_dir, current_col + self.forward_dir]
+                right_piece = self.board[current_row + self.forward_dir, current_col - self.forward_dir]
+                if !left_piece.empty? && left_piece.color != self.color
+                    moves << left_piece.pos
+                end 
+                if !right_piece.empty? && right_piece.color != self.color
+                    moves << right_piece.pos
                 end 
             end 
-            moves
-        end 
-
         moves
     end 
 
-    #   | O |   |
-    #   / |  \
     private 
 
     def at_start_row?
