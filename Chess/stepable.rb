e1ecf2cb2be92f591a8d1f5efc4b8cfa9701ab7f
Christian <cwooddel@u.rochester.edu>
2021-01-20 15:28:09 -0500

    Built on stepable class

--- Chess/stepable.rb
+++ Chess/stepable.rb
@@ -2,8 +2,21 @@ module Stepable
 
     def moves 
         moves = []
-        moves << self.move_diffs
-
+        current_row, current_col = self.pos
+        self.move_diffs.each do |diff|
+            dx, dy = diff
+            new_row = current_row + dx 
+            new_col = current_col + dy
+            new_pos = [new_row, new_col]
+            if new_pos[0] >= 0 || new_pos[0] <= 7
+                if self.board[new_pos].empty?
+                    moves << new_pos
+                end 
+            end 
+            moves << new_pos
+        end 
+        
+        
 
     end 
 
